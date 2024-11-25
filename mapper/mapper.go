b8c31229e26f4491f3f7bb5ff8e307688753e668
Sergei Kanaev <skanaev@ozon.ru>
2024-11-25 10:53:01 +0300

    add todos

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -8,15 +8,20 @@ import (
 	d "github.com/udisondev/go-mapping-jam/dto"
 )
 
-
+// TODO добавить обработку slice
+// TODO добавить обработку map
+// TODO добавить обработку pointer
+// TODO добавить обработку enum
+// TODO добавить source path
 type Mapper interface {
+	
 	//@qual={source="Firstname" target=".FirstName" mname="firstNameMapper"}
 	//@qual={source="Number" target=".Profile.Phone"}
-    MapPersonToDTO(p domain.Person) d.Person
+	MapPersonToDTO(p domain.Person) d.Person
 
 	//@qual={source="Firstname" target=".FirstName"}
 	//@qual={source="Phone" target=".Profile.Number"}
-    MapPersonToDomain(p d.Person) domain.Person
+	MapPersonToDomain(p d.Person) domain.Person
 }
 
 func firstNameMapper(firstName string) string {
